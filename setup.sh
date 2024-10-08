c41cc6ce565e2b40d90630b5289006199ce5746e
Shree Ram Abayankar Balaji <112720297+srabay16@users.noreply.github.com>
2024-10-08 13:03:08 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ After=display-manager.service network.target syslog.target
 [Service]
 Type=simple
 ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd adminadmin123
-User=c2srnano06
+ExecStop=/usr/bin/killall x11vnc
 Restart=on-failure
 
 [Install]
